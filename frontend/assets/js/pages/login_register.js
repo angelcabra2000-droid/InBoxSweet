1892066706446102ae503268df11c50ed5a45362
Angel Cabra <angelcabra2000@gmail.com>
2026-01-16 21:49:35 -0500

    pagina responsive, menos botones inicio de sesion y registro

--- frontend/assets/js/pages/login_register.js
+++ frontend/assets/js/pages/login_register.js
@@ -1,36 +1,33 @@
-const loginPanel = document.getElementById("loginPanel");
-const registerPanel = document.getElementById("registerPanel");
-const overlay = document.getElementById("overlay");
 
-/* Abrir login */
+
+
 function abrirLogin() {
+    mobilePanel?.classList.remove("active");
+    document.getElementById("hamburger")?.classList.remove("active");
+
     loginPanel.classList.add("active");
     registerPanel.classList.remove("active");
     overlay.classList.add("active");
 }
 
-/* Abrir register */
+
 function abrirRegister() {
     registerPanel.classList.add("active");
     loginPanel.classList.remove("active");
     overlay.classList.add("active");
 }
 
-/* Desde login → ir a register */
 function abrirRegisterDesdeLogin() {
     loginPanel.classList.remove("active");
     registerPanel.classList.add("active");
 }
 
-/* Desde register → volver a login */
 function volverALogin() {
     registerPanel.classList.remove("active");
     loginPanel.classList.add("active");
 }
 
-/* Cerrar todo */
-function cerrarPaneles() {
+function cerrarAuth() {
     loginPanel.classList.remove("active");
     registerPanel.classList.remove("active");
-    overlay.classList.remove("active");
 }
